074c6ee404363bb7a4ffb02f0020b02d88e34de6
Débora Camilly <87872533+deboraa07@users.noreply.github.com>
2022-05-07 10:30:13 -0300

    Alterando o comentário de main para start

--- Questao3.java
+++ Questao3.java
@@ -1,5 +1,5 @@
 public class Questao3 {
-//  public static void main(String[] args) { código da questão 5
+//  public static void start(String[] args) { código da questão 5
     public static void main(String[] args) {
         System.out.print("Terminei a primeira aula com um programa Java!");
     }
